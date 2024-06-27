51d3d044c9a3fc4e5313166aebc54d4b53ab68af
jhynsoo <jhynsoo@gmail.com>
2024-06-28 00:56:44 +0900

    chore: Fix build settings to use relative path

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-// import { defineConfig } from 'vitest/config';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-// import packageJson from './package.json';
 
 export default defineConfig({
   plugins: [react()],
-  // test: {
-  //   name: packageJson.name,
-  //   dir: './src',
-  //   globals: true,
-  //   // coverage: {
-  //   //   provider: 'istanbul',
-  //   //   include: ['src/**/*'],
-  //   // },
-  // },
+  base: './',
 });
